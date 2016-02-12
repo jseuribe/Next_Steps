7caa93f6aeced3c22d1a2a7d05fc60c45de8357f
jseuribe <jseuribe@hotmail.com>
2016-02-11 23:05:21 -0500

    Next's Step's slack gitbot sanity check
    Does the thing. (Tests our slack's github bot).

---
+++ project.java
@@ -0,0 +1,14 @@
+import BIG_DATA
+import complex_algorithm//it's complex
+
+
+public class next_steps{
+
+
+  public main(BIG_DATA SCHOOLS, BIG_DATA STUDENT)
+  {
+    recommendation = complex_algorithm(SCHOOLS, STUDENT);
+    return recommendation;
+  }
+
+}
