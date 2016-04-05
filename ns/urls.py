3d188321c12a3e094c0fa44bb4bd122872e6734d
jseuribe <jseuribe@hotmail.com>
2016-04-04 21:22:49 -0400

    Unchained Django

--- ns/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views #import the functions in views.py of ns
-
-app_name = 'ns'
-urlpatterns = [
-	url(r'^login/$', views.login, name='login'),
-	url(r'^home/$', views.index, name='index')
-]
