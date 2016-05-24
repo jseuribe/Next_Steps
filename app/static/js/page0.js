ed364a565c98acf6b52a52cc4cf30782a27d3eaf
wjustinu <wjustinu@gmail.com>
2016-05-24 18:02:00 -0400

    Add files via upload
    Javascript files

---
+++ app/static/js/page0.js
@@ -0,0 +1,56 @@
+	$(document).ready(function(){
+		<!--Check email input -->
+		$('#inputEmail').on('input', function() {
+			var input=$(this);
+			var validate = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+			var isEmail=validate.test(input.val());
+			if(isEmail){
+				input.removeClass("invalid").addClass("valid");
+			}
+			else{
+				input.removeClass("valid").addClass("invalid");
+			}
+		});
+
+		$('input').blur(function(){
+			var pass = $('input[name=Password]').val();
+			var repass = $('input[name=Repassword').val();
+
+			if(pass!=repass){
+				$('#inputPassword').removeClass("valid").addClass("invalid");
+				$('#inputRepassword').removeClass("valid").addClass("invalid");				
+			}
+
+			else{
+				$('#inputPassword').removeClass("invalid").addClass("valid");
+				$('#inputRepassword').removeClass("invalid").addClass("valid");		
+			}
+
+			if(($('input[name=Password]').val().length < 3) || ($('input[name=Repassword]').val().length < 3)){
+				$('#inputPassword').removeClass("valid").addClass("invalid");
+				$('#inputRepassword').removeClass("valid").addClass("invalid");
+			}
+		});
+
+		<!--Check whole form -->
+		$("#submitPage0").click(function(event){
+			var formData=$("#page0").serializeArray();
+			var zeroErrors=true;
+			for (var input in formData){
+				var element=$("#input"+formData[input]['name']);
+				var valid=element.hasClass("valid");
+				var errorAt=$("span", element.parent());
+				if (!valid){
+					errorAt.removeClass("error").addClass("errorShow");
+					element.removeClass("valid").addClass("invalid");
+					zeroErrors=false;
+				}
+				else{
+					errorAt.removeClass("errorShow").addClass("error");
+				}
+			}
+			if (!zeroErrors){
+				event.preventDefault(); 
+			}
+		});
+	});
---
+++ app/static/js/page3.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+	<!--Tuition must contain a value-->
+	$('#inputTuition').on('input', function() {
+		var input=$(this);
+		var tuition=input.val();
+		if(tuition && tuition != "tuition_limit_empty"){
+			input.removeClass("invalid").addClass("valid");
+		}
+		else{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});		
+
+	<!--State-->
+	$('#inputState').on('input', function() {
+		var input=$(this);
+		var state=input.val();
+		if(state && state != ""){
+			input.addClass("valid");
+		}
+		else{
+			input.removeClass("valid");
+		}
+	});	
+
+	<!--Check whole form -->
+	$("#submitPage3").click(function(event){
+		var formData=$("#page3").serializeArray();
+		var zeroErrors=true;
+
+		var prefCheck = jQuery("input[name=Pref]:checked").val();
+		var prefInput=$('#inputPref2');
+		var prefError=$("span", prefInput.parent());
+
+		if(!prefCheck){
+			prefError.removeClass("error").addClass("errorShow");
+		}
+		else{
+			prefError.removeClass("errorShow").addClass("error");
+		}
+
+		var costCheck = jQuery("input[name=Cost]:checked").val();
+		var costInput=$('#inputCost2');
+		var costError=$("span", costInput.parent());
+
+		if(!costCheck){
+			costError.removeClass("error").addClass("errorShow");
+		}
+		else{
+			costError.removeClass("errorShow").addClass("error");
+		}
+
+		var distCheck = jQuery("input[name=Dist]:checked").val();
+		var distInput=$('#inputDist2');
+		var distError=$("span", distInput.parent());
+
+		if(!distCheck){
+			distError.removeClass("error").addClass("errorShow");
+		}
+		else{
+			distError.removeClass("errorShow").addClass("error");
+		}
+
+		var acadCheck = jQuery("input[name=Acad]:checked").val();
+		var acadInput=$('#inputAcad2');
+		var acadError=$("span", acadInput.parent());
+
+		if(!acadCheck){
+			acadError.removeClass("error").addClass("errorShow");
+		}
+		else{
+			acadError.removeClass("errorShow").addClass("error");
+		}
+
+
+
+		for (var input in formData){
+			var element=$("#input"+formData[input]['name']);
+			var valid=element.hasClass("valid");
+			var errorAt=$("span", element.parent());
+			if (!valid){
+				errorAt.removeClass("error").addClass("errorShow");
+				element.removeClass("valid").addClass("invalid");
+				zeroErrors=false;
+			}
+			else{
+				errorAt.removeClass("errorShow").addClass("error");
+			}
+		}
+		if (!zeroErrors){
+			event.preventDefault(); 
+		}
+	});
+
+});
